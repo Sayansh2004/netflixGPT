1021d3a5472eae89711ecd96873fdf2f22b84c12
unknown <choprasayansh@gmail.com>
2026-02-14 16:26:20 +0530

    implemented useRef hook to handleForm data

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,25 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import Header from "./Header"
 
 export default function Login() {
   const[isSignIn,setIsSignIn]=useState();
-  const[formData,setFormData]=useState({
-    username:"",
-       email:"",
-       password:""
-  });
 
-
-
-
-
-  const handleInputChange=(e)=>{
-    const {name,value}=e.target;
-   
-    
-    setFormData((prev)={
-      ...prev,[name]:value
-    })
-    
-  }
+  const name=useRef(null);
+  const email=useRef(null);
+  const password=useRef(null);
+ 
 
   const toggleClick=()=>{
           setIsSignIn(!isSignIn);
+  }
+
+  const handleButtonClick=()=>{
+
+
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
   }
   return (
     <div>
@@ -32,17 +27,18 @@ export default function Login() {
         <div className="relative">
           <img src="https://assets.nflxext.com/ffe/siteui/vlv3/4371a395-0e42-46ae-be36-5755eebc638b/web/IN-en-20260209-TRIFECTA-perspective_3a6d8659-ddfe-4547-9584-dce64c02c230_medium.jpg" alt="bg-image" />
       <div className="absolute inset-0 flex items-center justify-center">
-  <form className="bg-black/80 text-white p-10 rounded-lg w-80 min-h-[420px] flex flex-col">
+  <form className="bg-black/80 text-white p-10 rounded-lg w-80 min-h-[420px] flex flex-col" onSubmit={handleSubmit}>
     
     <h1 className="font-bold text-2xl mb-6">{isSignIn?"Sign In":"Sign Up"}</h1>
 
     {!isSignIn &&
         <input
+        ref={name}
       type="text"
       name="username"
       placeholder="username"
-      value={formData.email}
-      onChange={handleInputChange}
+     
+      
       className="w-full p-3 mb-4 bg-gray-700 rounded outline-none"
     />
     }
@@ -51,21 +47,20 @@ export default function Login() {
       type="text"
       name="email"
       placeholder="Email"
-      value={formData.email}
-      onChange={handleInputChange}
+      ref={email}
+   
       className="w-full p-3 mb-4 bg-gray-700 rounded outline-none"
     />
 
     <input
-      type="text"
-      name="password"
+      type="password"
+    
       placeholder="Password"
-      value={formData.password}
-      onChange={handleInputChange}
+      ref={password}
       className="w-full p-3 mb-6 bg-gray-700 rounded outline-none"
     />
 
-    <button className="bg-red-700 py-3 rounded font-semibold hover:bg-red-600">
+    <button className="bg-red-700 py-3 rounded font-semibold hover:bg-red-600" onClick={handleButtonClick}>
     {isSignIn?"Sign In":"Sign Up"}
     </button>
 
